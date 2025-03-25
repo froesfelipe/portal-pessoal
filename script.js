f71d848989e4a16133569edb90fc32a37f3ad6e0
froesfelipe <fe-froes@hotmail.com>
2025-03-24 22:35:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function toggleMode() {
     const html = document.documentElement;
-       html.classList.toggle('light');
+    html.classList.toggle('light');
 
     const img = document.querySelector("#profile img");
 
